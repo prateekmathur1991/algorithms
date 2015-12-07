ba4deaa6251476a298a1a9c60406a79834d0fcf8
Prateek Mathur <prateekmathur1991@gmail.com>
2015-12-07 12:37:48 +0530

    MERGE SORT DONE

--- MergeSort.java
+++ MergeSort.java
@@ -6,15 +6,42 @@ import java.util.Arrays;
 
 class MergeSortDemo	{
 	public void mergeSort(int [] nums, int low, int high)	{
-		while (low < high)	{
-			int mid = (low + high)/2;
-			mergeSort(nums, mid, high);
+		if (low < high)	{
+			int mid = low + (high - low)/2;
+			mergeSort(nums, low, mid);
 			mergeSort(nums, mid + 1, high);
+			merge(nums, low, mid, high);
 		}
 	}
 
-	public void merge(int [] nums, int low, int mid1, int mid2, int high)	{
+	public void merge(int [] nums, int low, int mid, int high)	{
+		int [] helper = new int[nums.length];
+
+		for (int i = 0; i < nums.length; i++)	{
+			helper[i] = nums[i];
+		}
 		
+		int i = low;
+		int j = mid + 1;
+		int k = low;
+
+		while (i <= mid && j <= high)	{
+			if (helper[i] <= helper[j]) {
+				nums[k] = helper[i];
+				i++;
+			} else {
+				nums[k] = helper[j];
+				j++;
+			}
+
+			k++;
+		}
+
+		while (i <= mid) {
+			nums[k] = helper[i];
+			k++;
+			i++;
+		}
 	}
 }
 
@@ -25,11 +52,7 @@ class MergeSort	{
 		int [] nums = {5, 12, 4, 23, 78, 1, 6, 7, 16, 2};
 		System.out.println(Arrays.toString(nums));
 
-		int mid = (nums.length)/2;
-		demo.mergeSort(nums, 0, mid);
-		demo.mergeSort(nums, mid + 1, nums.length);
-		demo.merge(nums, 0, mid, mid + 1, nums.length);
-
+		demo.mergeSort(nums, 0, nums.length - 1);
 		System.out.println(Arrays.toString(nums));
 	}
 }
--- README.md
+++ README.md
@@ -2,9 +2,8 @@ My Practice with Data Structures and Algorithms
 
 Here is a list of incomplete/pending algorithms & data structures I need to implement-
 
-1. MergeSort
-2. Quick Select Algorithm
-3. Medians of Median Algorithm
-4. Queue & Priority Queues
-5. Heaps (Min Heaps & Max Heaps)
-6. Red Black Tree
+-> Quick Select Algorithm
+-> Medians of Median Algorithm
+-> Queue & Priority Queues
+-> Heaps (Min Heaps & Max Heaps)
+-> Red Black Tree

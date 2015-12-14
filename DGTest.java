0a256a5b7b9bdd2acf13bbe626dad5bb2f65d2ed
Prateek Mathur <prateekmathur1991@gmail.com>
2015-12-14 19:51:27 +0530

    Added directed graph implementation

---
+++ DGTest.java
@@ -0,0 +1,44 @@
+// Prateek Mathur
+
+// Java program to test a graph
+
+public class DGTest	{
+	public static void main(String [] args)	{
+		DGraph graph = new DGraph();
+		
+		graph.addVertex("r");
+		graph.addVertex("s");
+		graph.addVertex("t");
+		graph.addVertex("u");
+		graph.addVertex("v");
+		graph.addVertex("w");
+		graph.addVertex("x");
+		graph.addVertex("y");
+
+		graph.addEdge("r", "v");
+		graph.addEdge("r", "s");
+		graph.addEdge("s", "w");
+		graph.addEdge("w", "t");
+		graph.addEdge("w", "x");
+		graph.addEdge("t", "u");
+		graph.addEdge("t", "x");
+		graph.addEdge("u", "x");
+		graph.addEdge("u", "y");
+		graph.addEdge("x", "y");
+
+		/* System.out.println(graph.hasEdge("A", "B"));
+		System.out.println(graph.hasEdge("A", "C"));
+		System.out.println(graph.hasEdge("A", "H"));
+		System.out.println(graph.hasEdge("A", "G"));
+
+		System.out.println(graph.vertices + " " + graph.edges); */
+
+		graph.printAdjList();
+		/* graph.printBFS("r");
+		graph.printShortestPath("r", "x");
+
+		graph.printDFS();
+
+		graph.topologicalSort(); */
+	}
+}
---
+++ DGraph.java
@@ -0,0 +1,269 @@
+// Prateek Mathur
+
+// A directed graph implementation using Adjacency List representation
+
+import java.util.*;
+
+public class DGraph	{
+	// Inner class to represent a Vertex
+	class Vertex	{
+		public String name;
+		
+		// Required for BFS and DFS
+		public String color;
+		public int distance;
+		public Vertex parent;
+
+		// Required for DFS
+		public int discoveryTime;
+		public int finishTime;
+
+		Vertex(String name)	{
+			this.name = name;
+		}
+
+		@Override
+		public String toString() {
+			return this.name;
+		}
+	}
+
+	Map<Vertex, HashSet<Vertex>> adjList;
+	Map<String, Vertex> allVertices;
+	int vertices;
+	int edges;
+
+	// Global variable for depth-first search
+	private int time;
+
+	// LinkedList of Vertices, for topological sort
+	java.util.LinkedList<Vertex> verticesList;
+	
+	DGraph()	{
+		adjList = new HashMap<Vertex, HashSet<Vertex>>();
+		allVertices = new HashMap<String, Vertex>();
+		vertices = 0;
+		edges = 0;
+	}
+	
+	public void addVertex(String name)	{
+		Vertex v;
+		if ((v = allVertices.get(name)) == null) {
+			v = new Vertex(name);
+			allVertices.put(name, v);
+			adjList.put(v, new HashSet<Vertex>());
+			vertices++;
+		}
+	}
+
+	public boolean hasVertex(String name)	{
+		return allVertices.containsKey(name);
+	}
+
+	public void addEdge(String from, String to)	{
+		if (hasEdge(from, to)) {
+			return;
+		}
+
+		Vertex vf = null, vt = null;
+
+		if (!allVertices.containsKey(from))	{
+			vf = new Vertex(from);
+			allVertices.put(from, vf);
+			adjList.put(vf, new HashSet<Vertex>());
+			vertices++;
+		} else {
+			vf = allVertices.get(from);
+		}
+	
+		if (!allVertices.containsKey(to))	{	
+			vt = new Vertex(to);
+			allVertices.put(to, vt);
+			adjList.put(vt, new HashSet<Vertex>());
+			vertices++;
+		} else {
+			vt = allVertices.get(to);
+		}
+
+		
+		adjList.get(vf).add(vt);
+		edges++;
+	}
+
+	public boolean hasEdge(String from, String to)	{
+		if (!allVertices.containsKey(from) || !allVertices.containsKey(to))	{
+			return false;
+		}
+
+		Vertex vf = allVertices.get(from);
+		Vertex vt = allVertices.get(to);
+
+		return adjList.get(allVertices.get(from)).contains(allVertices.get(to));
+	}
+
+	public void printAdjList()	{
+		if (allVertices.size() == 0)	{
+			System.err.println("NO VERTICES");
+			return;
+		}
+
+		Iterator itr = allVertices.entrySet().iterator();
+
+		while (itr.hasNext())	{
+			Map.Entry pair = (Map.Entry) itr.next();
+			System.out.print(pair.getKey() + "-->");
+			
+			HashSet<Vertex> list = adjList.get(pair.getValue());
+			if (list.size() == 0)	{
+				System.out.println("END");
+				continue;
+			}	
+
+			Iterator itr1 = list.iterator();
+			while (itr1.hasNext())	{
+				Vertex v = (Vertex) itr1.next();
+				System.out.print(v.name + "-->");
+			}
+			System.out.print("END");
+			System.out.println();
+		}	
+	}
+
+	public void printBFS(String r)	{
+		if (!hasVertex(r))	{
+			System.err.println("ROOT NOT PRESENT IN TREE");
+			return;
+		}
+		
+		java.util.LinkedList<Vertex> queue = new java.util.LinkedList<Vertex>();
+		Vertex root = null, u= null;
+		
+		Iterator itr = allVertices.entrySet().iterator();
+		while (itr.hasNext())	{
+			Map.Entry entry = (Map.Entry) itr.next();
+			if (entry.getKey().equals(r)) {
+				root = (Vertex) entry.getValue();
+				continue;
+			}
+
+			u = (Vertex) entry.getValue();
+			u.color = "w";
+			u.distance = Integer.MAX_VALUE;
+			u.parent = null;
+		}
+
+		root.color = "g";
+		root.distance = 0;
+		root.parent = null;
+
+		queue.add(root);
+
+		while (queue.size() != 0)	{
+			System.out.println("Queue right now:: " + Arrays.toString(queue.toArray(new Vertex[0])));
+			u = queue.poll();
+			HashSet<Vertex> list = adjList.get(u);
+
+			System.out.println("Getting and coloring Neighbours");
+			Iterator itr1 = list.iterator();
+			while (itr1.hasNext())	{
+				Vertex v = (Vertex) itr1.next();
+				if ("w".equals(v.color))	{
+					System.out.println("Got white neighbour " + v.name);
+					v.color = "g";
+					v.distance = u.distance + 1;
+					v.parent = u;
+					System.out.println("Processed neighbour. Name:: " + v.name + " Distance:: " + v.distance + " Parent:: " + u.name);
+					queue.add(v);
+				}
+			}
+
+			u.color = "b";
+			System.out.println();
+		}	
+	}
+
+	public void printShortestPath(String source, String vertex)	{
+		if (!hasVertex(source) || !hasVertex(vertex))	{
+			System.err.println("Source or vertex not present in graph");
+			return;
+		}
+
+		this.printBFS(source);
+
+		Vertex s = allVertices.get(source);
+		Vertex v = allVertices.get(vertex);
+		
+		if (source.equals(vertex))	{
+			System.out.print(source + " ");
+		} else if (v.parent == null)	{
+			System.err.println("No path from source to vertex");
+		} else {
+			printShortestPath(source, v.parent.name);
+			System.out.print(v.name + " ");
+		}
+	}
+	
+	public void printDFS()	{
+		DFS(false);
+	}
+
+	private void DFS(boolean topo)	{
+		if (topo) {
+			verticesList = new java.util.LinkedList<Vertex>();
+		}
+
+		Iterator itr = allVertices.entrySet().iterator();
+		while (itr.hasNext())	{
+			Map.Entry entry = (Map.Entry) itr.next();
+			Vertex u = (Vertex) entry.getValue();
+			u.color = "w";
+			u.parent = null;
+			u.discoveryTime = 0;
+			u.finishTime = 0;
+		}
+
+		this.time = 0;
+		
+		itr = allVertices.entrySet().iterator();
+		while (itr.hasNext())	{
+			Map.Entry entry = (Map.Entry) itr.next();
+			Vertex u = (Vertex) entry.getValue();
+			System.out.println("About to visit:: " + u.name);
+			dfsVisit(u, topo);
+		}
+	}
+
+	private void dfsVisit(Vertex u, boolean topo)	{
+		time++;
+		u.discoveryTime = time;
+		u.color = "g";
+
+		HashSet<Vertex> list = adjList.get(u);
+		Iterator itr = list.iterator();
+		System.out.println("Visiting adjacency list for " + u.name);
+
+		while (itr.hasNext())	{
+			Vertex v = (Vertex) itr.next();
+			if (v.color.equals("w"))	{
+				System.out.println("White node found. About to visit recursively " + v.name);
+				v.parent = u;
+				dfsVisit(v, topo);
+			}
+		}
+
+		u.color = "b";
+		time++;
+		u.finishTime = time;
+
+		System.out.println("Visit complete for " + u.name + " First Visit Time:: " + u.discoveryTime + " Last Visit Time:: " + u.finishTime);
+	
+		if (topo) {
+			verticesList.addFirst(u);
+		}
+	}
+
+	/* public void topologicalSort()	{
+		DFS(true);
+		System.out.println(Arrays.toString(verticesList.toArray(new Vertex[0])));
+	} */
+}

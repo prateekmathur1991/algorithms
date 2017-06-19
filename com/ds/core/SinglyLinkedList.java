70fc8fb469025a05202fbde68f8dc14de261dd2d
Prateek Mathur <prateekmathur1991@gmail.com>
2017-06-19 09:21:28 +0530

    Singly linked list is generic

--- com/ds/core/SinglyLinkedList.java
+++ com/ds/core/SinglyLinkedList.java
@@ -4,25 +4,20 @@ public class SinglyLinkedList<T> {
 
 	// Inner class to represent a node
 	class Node	{
-		public int num;
+		public T data;
 		public Node next;
 
-		Node()	{
-			this.num = 0;
-			this.next = null;
-		}
-
-		Node(int num)	{
-			this.num = num;
+		Node(T num)	{
+			this.data = num;
 			this.next = null;
 		}
 
 		@Override
 		public String toString()	{
 			StringBuilder builder = new StringBuilder();
-			builder.append(this.num);
+			builder.append(this.data);
 			if (null != this.next) {
-				builder.append("--->").append(this.next.num);
+				builder.append("--->").append(this.next.data);
 			}
 			return builder.toString();
 		}
@@ -35,7 +30,7 @@ public class SinglyLinkedList<T> {
 		start = null;
 	}
 
-	public void add(int key) {
+	public void add(T key) {
 		Node node = new Node(key);
 
 		if (null == start) {
@@ -51,19 +46,19 @@ public class SinglyLinkedList<T> {
 		current.next = node;
 	}
 
-	public String search(int key)	{
+	public String search(T key)	{
 		Node getNode = searchNode(key);
 		return getNode == null ? "NOT FOUND" : getNode.toString();
 	}
 
-	private Node searchNode(int key)	{
+	private Node searchNode(T key)	{
 		if (null == start) {
 			return null;
 		}
 
 		Node current = start;
 		while (null != current)	{
-			if (key == current.num) {
+			if (key == current.data) {
 				return current;
 			}
 
@@ -73,7 +68,7 @@ public class SinglyLinkedList<T> {
 		return null;
 	}
 
-	public void insertAfter(int value, int key)	{
+	public void insertAfter(T value, T key)	{
 		if (null == start)	{
 			System.err.println("LIST EMPTY");
 			return;
@@ -91,7 +86,7 @@ public class SinglyLinkedList<T> {
 		node.next = temp;
 	}
 
-	public void insertBefore(int value, int key)	{
+	public void insertBefore(T value, T key)	{
 		if (null == start)	{
 			System.err.println("LIST EMPTY");
 			return;
@@ -100,10 +95,10 @@ public class SinglyLinkedList<T> {
 		this.start = insertRecordBefore(value, key);
 	}
 
-	private Node insertRecordBefore(int value, int key)	{
+	private Node insertRecordBefore(T value, T key)	{
 		Node node = new Node(key);
 
-		if (value == this.start.num)	{
+		if (value == this.start.data)	{
 			Node temp = this.start;
 			this.start = node;
 			node.next = temp;
@@ -111,12 +106,12 @@ public class SinglyLinkedList<T> {
 			Node current = start;
 			Node prev = null;
 
-			while (null != current.next && current.num != value)	{
+			while (null != current.next && current.data != value)	{
 				prev = current;
 				current = current.next;
 			}
 		
-			if (current.num == value)	{
+			if (current.data == value)	{
 				prev.next = node;
 				node.next = current;
 			}
@@ -125,7 +120,7 @@ public class SinglyLinkedList<T> {
 		return this.start;		
 	}
 
-	public void delete(int key)	{		
+	public void delete(T key)	{		
 		if (null == start)	{
 			System.err.println("LIST EMPTY");
 			return;
@@ -134,20 +129,20 @@ public class SinglyLinkedList<T> {
 		this.start = deleteRecord(key);	
 	}
 
-	private Node deleteRecord(int key)	{
-		if (key == this.start.num)	{
+	private Node deleteRecord(T key)	{
+		if (key == this.start.data)	{
 			start = start.next;
 		} else {
 			Node current = start;
 			Node prev = null;
 		
-			while (null != current.next && current.num != key)	{
+			while (null != current.next && current.data != key)	{
 				prev = current;
 				current = current.next;
 			}
 
 			
-			if (current.num == key)	{
+			if (current.data == key)	{
 				prev.next = current.next;
 				current = null;
 			}
@@ -164,58 +159,13 @@ public class SinglyLinkedList<T> {
 
 		Node current = start;
 		while (null != current)	{
-			System.out.print(Integer.toString(current.num) + "--->");
+			System.out.print(current.data.toString() + "--->");
 			current = current.next;
 		}
 		
 		System.out.println("END");
 	}
 	
-	/* public void removeDuplicates()	{
-		removeDups(start);
-	}
-	
-	private void removeDups(Node node)	{
-		HashSet<Integer> set = new HashSet<Integer>();
-		Node prev = null;
-		
-		while (null != node)	{
-			if (set.contains(node.num))	{
-				prev.next = node.next;
-			} else {
-				set.add(node.num);
-				prev = node;
-			}
-			
-			node = node.next;
-		}
-	}	
-
-	public int findKthToLast(int k)	{
-		Node n = kthToLast(start, k);
-		return n == null ? Integer.MIN_VALUE : n.num;
-	}
-
-	private Node kthToLast(Node start, int k)	{
-		Node p1 = start;
-		Node p2 = start;
-
-		for (int i = 0; i < k; i++)	{
-			if (null == p1)	{
-				return null;
-			}
-
-			p1 = p1.next;
-		}
-
-		while (null != p1)	{
-			p1 = p1.next;
-			p2 = p2.next;
-		}
-
-		return p2;
-	} */
-
 	public void printRecursively()	{
 		traverse(start);	
 	}
@@ -223,17 +173,17 @@ public class SinglyLinkedList<T> {
 	// Recursively traverses through the list and prints each element
 	private void traverse(Node node)	{
 		if (null == node.next)	{
-			System.out.println(Integer.toString(node.num) + "--->END");
+			System.out.println(node.data.toString() + "--->END");
 			return;
 		} else {
-			System.out.print(Integer.toString(node.num) + "--->");
+			System.out.print(node.data.toString() + "--->");
 			traverse(node.next);
 		}
 	}
 
-	public int findMiddle()	{
+	public T findMiddle()	{
 		Node middle = findMiddle(this.start);
-		return middle.num;
+		return middle.data;
 	}
 
 	private Node findMiddle(Node start)	{		
